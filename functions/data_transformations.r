990ea737855121157bd07c752e08014e583ae414
Håvard Wikse <havard.wikse2@student.nhh.no>
2023-10-16 11:47:46 +0200

    finished assignment

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,4 +1,10 @@
-# functions/data_transformations.r
+#
+# Functions
+#
+
+
+
+# Function that transforms stations_metadata into a easier format to follow 
 
 transform_metadata_to_df <- function(stations_metadata) {
   df <- stations_metadata[[1]] %>%
@@ -6,6 +12,7 @@ transform_metadata_to_df <- function(stations_metadata) {
     list_rbind() %>%
     mutate(latestData = map_chr(latestData, 1, .default = NA_character_)) %>%
     mutate(latestData = as_datetime(latestData, tz = "Europe/Berlin")) %>%
+    mutate(latestData = with_tz(latestData, "UTC")) %>% 
     mutate(location = map(location, unlist)) %>%
     mutate(
       lat = map_dbl(location, "latLon.lat"),
@@ -13,6 +20,31 @@ transform_metadata_to_df <- function(stations_metadata) {
     ) %>%
     select(-location)
   
+  return(df)
+}
+
+
+# Time-function for task 
+
+to_iso8601 <- 
+  function(datetime_var, offset_in_days){
+    adjusted_datetime <- datetime_var + days(offset_in_days)
+    iso8601_string <- format(with_tz(adjusted_datetime, "UTC"), 
+                             "%Y-%m-%dT%H:%M:%SZ")
+    return(iso8601_string)
+  }
+
+# Transforming volumes
+
+transform_volumes <- function(data) {
+  volumes <- data$trafficData$volume$byHour$edges
+  df <- purrr::map_dfr(volumes, ~{
+    tibble::tibble(
+      from = .x$node$from,
+      to = .x$node$to,
+      volume = .x$node$total$volumeNumbers$volume
+    )
+  })
   return(df)
 }
 
