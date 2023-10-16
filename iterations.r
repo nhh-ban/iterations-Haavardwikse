07e745a6dad7321603c2eb4955f5a513274a4558
Håvard Wikse <havard.wikse2@student.nhh.no>
2023-10-16 12:07:45 +0200

    fixing plot

--- iterations.r
+++ iterations.r
@@ -65,6 +65,28 @@ stations_metadata_df %>%
   geom_line() + 
   theme_classic()
 
+#improving plot
+stations_metadata_df %>% 
+  filter(latestData > Sys.Date() - days(7)) %>% 
+  sample_n(1) -> sampled_station
 
+station_name <- sampled_station$name
+
+sampled_station %$% 
+  vol_qry(
+    id = id,
+    from = to_iso8601(latestData, -4),
+    to = to_iso8601(latestData, 0)
+  ) %>% 
+  GQL(., .url = configs$vegvesen_url) %>%
+  transform_volumes() %>% 
+  mutate(from = as.POSIXct(from, format="%Y-%m-%dT%H:%M:%S")) %>%
+  ggplot(aes(x=from, y=volume, group = 1)) + 
+  geom_line(aes(color = station_name)) + 
+  geom_point(aes(color = station_name), size = 2, alpha = 0.7) +  # Adjust point size and transparency
+  scale_color_brewer(palette = "Set2") +  # Using a Brewer palette for pleasant colors
+  theme_minimal() +  # Use a minimal theme for a clean look
+  labs(color = 'Traffic Station') + 
+  scale_x_datetime(labels = scales::date_format("%Y-%m-%d %H:%M")) 
 
 
