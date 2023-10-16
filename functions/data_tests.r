0d67016b645c0e217ca1244b876ddf039858fbfe
Håvard Wikse <havard.wikse2@student.nhh.no>
2023-10-16 11:50:51 +0200

    Shortened the descriptions

--- functions/data_tests.r
+++ functions/data_tests.r
@@ -1,105 +1,60 @@
-# This file contains tests to be applied to 
-# the Vegvesen stations-data *after* being transformed
-# to a data frame. 
-# 
-# All tests are packed in a function test_stations_metadata that apples
-# all the aforementioned tests
-
-
-# The purpose of this function is to check if the column names in the new data-
-# frame is correct. It compares the actual names to the expected names, and 
-# returns either PASS og FAIL, with a comment to each answer
-
-test_stations_metadata_colnames <-
-  function(df) {
-    
-    expected_colnames <- c("id", "name", "latestData", "lat", "lon")
-    
-    if (all(colnames(df) == expected_colnames) == TRUE) {
-      print("PASS: Data has the correct columns")
-    } else{
-      print("FAIL: Columns do not match the correct specification")
-    }
+# Check if column names match the expected names.
+test_stations_metadata_colnames <- function(df) {
+  expected_colnames <- c("id", "name", "latestData", "lat", "lon")
+  if (all(colnames(df) == expected_colnames) == TRUE) {
+    print("PASS: Data has the correct columns")
+  } else {
+    print("FAIL: Columns do not match the correct specification")
   }
+}
 
-
-# This function tests that the dataframe has a reasonable number of rows. It 
-# defines that number to be between 5 000 and 10 000. If the number of rows
-# is not between these numbers, it returns FAIL and an explaination. If the 
-# number of rows is between, it returns PASS
-
-test_stations_metadata_nrows <-
-  function(df) {
-    
-    min_expected_rows <- 5000
-    max_expected_rows <- 10000
-    
-    if (nrow(df) > min_expected_rows & nrow(df) < max_expected_rows) {
-      print("PASS: Data has a reasonable number of rows")
-    } else if (nrow(df) <= min_expected_rows) {
-      print("FAIL: Data has suspiciously few rows")
-    } else {
-      print("FAIL: Data has suspiciously many rows")
-    }
+# Validate row count is between 5,000 and 10,000.
+test_stations_metadata_nrows <- function(df) {
+  min_expected_rows <- 5000
+  max_expected_rows <- 10000
+  if (nrow(df) > min_expected_rows & nrow(df) < max_expected_rows) {
+    print("PASS: Data has a reasonable number of rows")
+  } else if (nrow(df) <= min_expected_rows) {
+    print("FAIL: Data has suspiciously few rows")
+  } else {
+    print("FAIL: Data has suspiciously many rows")
   }
+}
 
-
-# This function checks if the columns in the dataframe have the expected data 
-# types. It has defines a vector of expectes data types, and compares them to 
-# the actual df. It prints the answer PASS or FAIL, depending on if there is 
-# a match or not
-
-test_stations_metadata_coltypes <-
-  function(df) {
-    expected_coltypes <-
-      c("character", "character", "double", "double", "double")
-    
-    if (all(df %>%
-            map_chr( ~ typeof(.)) == expected_coltypes) == TRUE) {
-      print("PASS: All cols have the correct specifications")
-    } else{
-      print("FAIL: Columns do not have the correct specification")
-    }
+# Ensure columns have the expected data types.
+test_stations_metadata_coltypes <- function(df) {
+  expected_coltypes <- c("character", "character", "double", "double", "double")
+  if (all(sapply(df, typeof) == expected_coltypes) == TRUE) {
+    print("PASS: All cols have the correct specifications")
+  } else {
+    print("FAIL: Columns do not have the correct specification")
   }
+}
 
-
-# This function tests if the df is missing too much data from the metadata. It 
-# sets the max of missing values to 200. If we are missing more than 200 values,
-# the df has not passed the test. 
-
-test_stations_metadata_nmissing <-
-  function(df) {
-    max_miss_vals <- 200
-    
-    if (df %>% map_int( ~ sum(is.na((.)))) %>% sum(.) < max_miss_vals) {
-      print("PASS: Amount of missing values is reasonable")
-    } else {
-      print("FAIL: Too many missing values in data set")
-    }
+# Check if data has more than 200 missing values.
+test_stations_metadata_nmissing <- function(df) {
+  max_miss_vals <- 200
+  if (sum(sapply(df, function(col) sum(is.na(col)))) < max_miss_vals) {
+    print("PASS: Amount of missing values is reasonable")
+  } else {
+    print("FAIL: Too many missing values in data set")
   }
+}
 
-
-# This function tests if the column for time is of the right format. The 
-# expected format is UTC, and it compares this to the df, and prints the 
-# answer
-
-test_stations_metadata_latestdata_timezone <-
-  function(df) {
-    
-    if (attr(df$latestData,"tzone")=="UTC") {
-      print("PASS: latestData has UTC-time zone")
-    } else {
-      print("FAIL: latestData does not have expected UTC-time zone")
-    }
+# Verify time column is in UTC time zone.
+test_stations_metadata_latestdata_timezone <- function(df) {
+  if (attr(df$latestData,"tzone") == "UTC") {
+    print("PASS: latestData has UTC-time zone")
+  } else {
+    print("FAIL: latestData does not have expected UTC-time zone")
   }
+}
 
-
-# This funciton combines all the other functions
-test_stations_metadata <- 
-  function(df){
-    test_stations_metadata_colnames(df)
-    test_stations_metadata_coltypes(df)
-    test_stations_metadata_nmissing(df)
-    test_stations_metadata_nrows(df)
-    test_stations_metadata_latestdata_timezone(df)
-  }
+# Combine and run all the above tests.
+test_stations_metadata <- function(df) {
+  test_stations_metadata_colnames(df)
+  test_stations_metadata_coltypes(df)
+  test_stations_metadata_nmissing(df)
+  test_stations_metadata_nrows(df)
+  test_stations_metadata_latestdata_timezone(df)
+}
